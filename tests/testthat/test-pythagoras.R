fa05ca3c0a7b072b9ac3bd961b45f36e09ee0c9e
FEMKE THON <femke_thon@hotmail.com>
2016-04-26 12:21:09 +0200

    fixed the OR statement in the function

--- tests/testthat/test-pythagoras.R
+++ tests/testthat/test-pythagoras.R
@@ -2,7 +2,6 @@ context("pythagoras")
 
 test_that("Pythagoras works", {
   expect_equal(Pythagoras(3, 4), 5)
-  expect_error(Pythagoras("Nonsense", "More nonsense"),
-    "Pythagoras: must have two values for arguments"
-  )
+  expect_error(Pythagoras(),
+               c("Pythagoras: must have two values for arguments"))
 })
